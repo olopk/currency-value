75ee0097c69730fb6f0831eff0715b185e2d1274
olopk <olekwojas@gmail.com>
2020-07-01 21:24:12 +0200

    prepared reducer and actiontypes.

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,44 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    currencies: [],
+    favourites: [],
+    loading: false,
+    error: null
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case actionTypes.FETCH_CURRENCIES_START:
+            return{
+                ...state,
+                loading: true,
+                error: null
+            }
+        case actionTypes.FETCH_CURRENCIES_SUCCESS:
+            return{
+                ...state,
+                currencies: action.currencies,
+                loading: false,
+                error: null
+            }
+        case actionTypes.FETCH_CURRENCIES_FAIL:
+            return{
+                ...state,
+                loading: false,
+                error: action.error
+            }
+        case actionTypes.ADD_FAVOURITE:
+            return{
+                ...state,
+                favourites: state.favourites.find(el => el.code === action.currency.code) ? state.favourites : state.favourites.push(action.currency)
+            }
+        case actionTypes.REM_FAVOURITE:
+            return{
+                ...state,
+                favourites: state.favourites.filter(el => el.code !== action.currency.code)
+            }
+    }
+}
+
+export default reducer;
