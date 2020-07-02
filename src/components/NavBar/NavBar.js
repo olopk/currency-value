93986e6021712bcacd2ba99ad298d4cfdf80db06
olopk <olekwojas@gmail.com>
2020-07-02 11:26:47 +0200

    React router connection for navbar tabs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import {Link} from 'react-router-dom';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import EuroIcon from '@material-ui/icons/Euro';
 import {makeStyles} from '@material-ui/styles';
@@ -14,13 +15,13 @@ const useStyles = makeStyles({
 })
 
 export default function DisabledTabs() {
-  const [value, setValue] = React.useState(2);
+  const [value, setValue] = React.useState(1);
 
   const classes = useStyles();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
-  };
+  }
 
   return (
     <Paper square>
@@ -32,8 +33,8 @@ export default function DisabledTabs() {
         className={classes.tabs}
         centered
       >
-        <Tab label="Currencies" icon={<EuroIcon />}/>
-        <Tab label="Favourite" icon={<FavoriteIcon />}/>
+        <Tab label="Currencies" value={1} icon={<EuroIcon />} component={Link} to='/'/>
+        <Tab label="Favourite" value={2} icon={<FavoriteIcon/>} component={Link} to='/favourites'/> 
       </Tabs>
     </Paper>
   );
