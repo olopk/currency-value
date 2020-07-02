31c24dcd7b6063a93489d4529073116672b7b1b4
olopk <olekwojas@gmail.com>
2020-07-02 11:26:26 +0200

    proper MainContent rendering + props passing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Container from '@material-ui/core/Container';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 import DataContainer from './DataContainer/DataContainer';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {fetchCurrencies} from '../store/actions/currencies_fetching';
 
 export default function Main() {
 
-  const error = useSelector(state => state.error)
+  const currencies = useSelector(state => state.currencies)
+  const favourites = useSelector(state => state.favourites)
+  const fetchData = useDispatch()
 
-  console.log(error)
+  useEffect(()=>{
+  if(currencies.length === 0){
+       fetchData(fetchCurrencies())
+    }
+  },[currencies, fetchData])
 
   return (
     <BrowserRouter>
       <Container fixed>
           <NavBar/>
           <Switch>
-              <Route path='/favourites' render={()=> {
-                //   some component to render
-              }}/>
+              <Route path='/favourites' render={()=>(
+                <DataContainer
+                    type='favourites'
+                    currencies={favourites}
+                />
+                )}/>
               <Route path='/' render={()=>(
-                  <DataContainer/>
+                  <DataContainer
+                     type='currencies'
+                     currencies={currencies}
+                     favourites={favourites}
+                  />
               )}/>
           </Switch>
 
